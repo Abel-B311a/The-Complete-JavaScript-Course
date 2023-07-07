d3f4c24c01aee56df0ae2db074c524d0b1bc0853
Abel-B3lla <bel8794@gmail.com>
2023-07-07 15:30:09 +0300

    challenge 2

--- 01-Fundamentals-Part-1/starter/codingChallenge2.js
+++ 01-Fundamentals-Part-1/starter/codingChallenge2.js
@@ -4,10 +4,13 @@ const koalasAVG = (88+91+110)/3
 console.log(`The average score of Team Dolphine is ${dolphinesAVG}`)
 console.log(`The average score of Team Koalas is ${koalasAVG}`)
 
-if (dolphinesAVG > koalasAVG) {
+if ((dolphinesAVG > koalasAVG) && dolphinesAVG >= 100) {
     console.log(`Dolphines WIN!`)
-} else if (dolphinesAVG < koalasAVG) {
+} else if ((dolphinesAVG < koalasAVG) && koalasAVG >= 100) {
     console.log(`Koalas WIN!`)
+} else if ((dolphinesAVG == koalasAVG) && ((dolphinesAVG && koalasAVG) >= 100)) {
+    console.log(`DRAW!`)
 } else {
-    console.log(`Draw`)
+    console.log(`Nobody Wins`)
 }
+
