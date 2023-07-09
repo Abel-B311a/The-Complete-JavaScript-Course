4315f3302c00739922a346cfb56e00d1ba9505b4
Abel-B3lla <bel8794@gmail.com>
2023-07-09 22:33:13 +0300

    update

--- 01-Fundamentals-Part-1/starter/codingChallenge3.js
+++ 01-Fundamentals-Part-1/starter/codingChallenge3.js
@@ -1,6 +1,6 @@
-let bill = 275
-const tip1 = (bill*0.15)
-const tip2 = (bill*0.2)
+let bill = 275;
+const tip1 = (bill*0.15);
+const tip2 = (bill*0.2);
 
 bill >= 50 && bill <= 300 ? console.log(`Your Bill is $${bill}, Your Tip is $${tip1}, Total = $${bill+tip1}`): console.log(`Your Bill is $${bill}, Your Tip is $${tip2}, Total = $${bill+tip2}`)
 
