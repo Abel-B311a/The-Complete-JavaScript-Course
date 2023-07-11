14a1820d7cd75e744eca9642bc39a3d01c9ef2be
Abel-B3lla <bel8794@gmail.com>
2023-07-11 23:10:36 +0300

    arrow function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -16,3 +16,41 @@ function bella(Age, highlight) {
 
 console.log(bella(18, `went to AAU`))
 console.log(bella(24, `landed his 1st Job`))
+console.log(bella(29, `changed his career path to a Frontend Developer`))
+
+function bella(Age, highlight) { // function name and variables
+  return `When bella reached the age ${Age}, he ${highlight}.`;} // you can directlly return it no need to define a variable and then return the variable 
+console.log(bella(18, `went to AAU`))  // the values are called arguments 
+console.log(bella(24, `landed his 1st Job`))
+
+function ageCalDec(birthYear) {
+  return 2023-birthYear
+}
+
+const ageBella = ageCalDec(1994)
+const ageAshe = ageCalDec(1995)
+console.log(ageBella, ageAshe)
+
+
+
+const ageCalExp = function (birthYear) {
+  return 2023-birthYear
+}
+
+const ageABD = ageCalExp(1994)
+const ageBoka = ageCalExp(1996)
+console.log(ageABD, ageBoka)
+
+const ageCalArr = birthYear => 2023-birthYear
+const ageNahom = ageCalArr(1994)
+console.log(ageNahom)
+
+const retirement = (birthYear, Nameof) => {
+  const amet = 2023-birthYear;
+  const ret = 60-amet;
+  return `${Nameof} retires in ${ret} years!` 
+}
+
+const bellaRetData = retirement(1994, `Bella`)
+const MotherRerData = retirement(1965, `Messi`)
+console.log(`${bellaRetData} and his mother, ${MotherRerData}`)
