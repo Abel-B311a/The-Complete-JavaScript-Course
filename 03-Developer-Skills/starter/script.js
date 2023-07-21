0f0f4c9e66508732c30a141ca355d86af169f731
Abel-B3lla <bel8794@gmail.com>
2023-07-21 13:33:30 +0300

    Max Min in an Array

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// const x = 3;
+// console.log();
+let temps = [3, -2, -6, -1, `error`, 9, 13, 17, 15, 14, 9, 5];
+
+const calcTemp = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    if (typeof temps[i] !== `number`) continue;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  return max - min;
+};
+const amp = calcTemp(temps);
+console.log(amp);
