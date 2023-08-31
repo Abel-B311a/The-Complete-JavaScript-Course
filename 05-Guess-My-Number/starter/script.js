43b123045e6245be3129beefe5cb6991a949b196
Abel-B3lla <bel8794@gmail.com>
2023-08-31 22:40:45 +0300

    DOM Pilot

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector(`.message`).textContent);
+
+document.querySelector(`.message`).textContent = `Play Around!`;
+
+document.querySelector(`.number`).textContent = 10;
+document.querySelector(`.score`).textContent = 5;
+
+document.querySelector(`.guess`).value = 5;
