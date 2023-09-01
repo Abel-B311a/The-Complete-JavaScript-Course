6642420f9e31bce205088c94ce52b24a4739a013
Abel-B3lla <bel8794@gmail.com>
2023-09-01 15:41:53 +0300

    Guessing Game!

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,10 +1,29 @@
 'use strict';
 
-console.log(document.querySelector(`.message`).textContent);
+// console.log(document.querySelector(`.message`).textContent);
 
-document.querySelector(`.message`).textContent = `Play Around!`;
+// document.querySelector(`.message`).textContent = `Play Around!`;
 
-document.querySelector(`.number`).textContent = 10;
-document.querySelector(`.score`).textContent = 5;
+// document.querySelector(`.number`).textContent = 10;
+// document.querySelector(`.score`).textContent = 5;
 
-document.querySelector(`.guess`).value = 5;
+// document.querySelector(`.guess`).value = 5;
+
+const secretNumber = Math.trunc(Math.random()*20)+1
+document.querySelector(`.number`).textContent = secretNumber
+
+document.querySelector(`.check`).addEventListener(`click`, 
+function() {
+    const guess = Number(document.querySelector(`.guess`).value)
+   
+    if (!guess){
+        document.querySelector(`.message`).textContent = `🛑 No Number!`
+    } else if (guess === secretNumber) {
+        document.querySelector(`.message`).textContent = `🎉 Correct Number!`
+    }
+    else if (guess > secretNumber) {
+        document.querySelector(`.message`).textContent = `Too high`
+    }
+}
+)
+
