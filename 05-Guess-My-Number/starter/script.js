68f9ab5c5f88755b74ac7761cf989756cd51c9bd
Abel-B3lla <bel8794@gmail.com>
2023-09-01 21:00:17 +0300

    Game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,26 +9,35 @@
 
 // document.querySelector(`.guess`).value = 5;
 
-const secretNumber = Math.trunc(Math.random()*20)+1
-document.querySelector(`.number`).textContent = secretNumber
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(`.number`).textContent = secretNumber;
 
-document.querySelector(`.check`).addEventListener(`click`, 
-function() {
-    const guess = Number(document.querySelector(`.guess`).value)
-   
-    if (!guess){
-        document.querySelector(`.message`).textContent = `🛑 No Number!`
-    } else if (guess === secretNumber) {
-        document.querySelector(`.message`).textContent = `🎉 Correct Number!`
-    }
-    else if (guess > secretNumber) {
-        document.querySelector(`.message`).textContent = `Too high`
-    }
-    else if (guess < secretNumber < 21) {
-        document.querySelector(`.message`).textContent = `Too low` 
+let score = 20;
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+
+  if (guess > 20 || guess <= 0) {
+    document.querySelector(`.message`).textContent = `🛑 Not in Range!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
+  } else if (guess > secretNumber && guess < 21) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `😔 Too high!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
     } else {
-        document.querySelector(`.message`).textContent = `Not in range`
+      document.querySelector(`.score`).textContent = 0;
+      document.querySelector(`.message`).textContent = `🥵 You lost the Game`;
     }
-}
-)
-
+  } else if (guess < secretNumber && guess > 0) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `😔 Too low!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.score`).textContent = 0;
+      document.querySelector(`.message`).textContent = `🥵 You lost the Game`;
+    }
+  }
+});
